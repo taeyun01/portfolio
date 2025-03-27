dabbc56614614fbbeb4d1f1ebc6040e248914299
taeyun <lwe12f675@gmail.com>
2025-03-27 19:47:33 +0900

    feat: text

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,33 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>배포완료</h1>
+      <h1>소개</h1>
+      <p>프로필사진</p>
+      <p>유태윤</p>
+      <p>소개 하기... 안녕하세요...</p>
+      <hr />
+
+      <h1>사용 스택</h1>
+      <p>React, TypeScript, Tailwind CSS 등등 (아이콘 찾아서 넣기)</p>
+      <hr />
+
+      <h1>프로젝트 소개</h1>
+      <h2>React</h2>
+      <p>
+        <a href="https://booking-seven-chi.vercel.app">Booking 프로젝트</a>
+      </p>
+      <p>
+        <a href="https://work-connect-plum.vercel.app">워크커넥트 프로젝트</a>
+      </p>
+      <p>
+        <a href="https://bloodfolio.vercel.app">블러드폴리오 프로젝트</a>
+      </p>
+
+      <h2>Next.js</h2>
+      <p>
+        <a href="https://next13-project-mu.vercel.app/">자산관리 프로젝트</a>
+      </p>
+      <hr />
     </>
   );
 }
